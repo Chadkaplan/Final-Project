108d57ec1e6da2f39c3bd0c39266032417c02ede
Chad Kaplan <chadkaplan@knights.ucf.edu>
2019-11-07 11:37:46 -0500

    Added a stable background image

--- client/src/pages/PublicRoute/Main/index.js
+++ client/src/pages/PublicRoute/Main/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
+import "./style.css";
 
 const Main = (props) => {
   const jumboStyle= {
-    backgroundColor: "#FFFF33"
-  }
-  const titleColor={
+    backgroundColor: "#FFFF33",
     color: "#001f3f"
   }
   return (
     <div>
+      <background>
       <Jumbotron style={jumboStyle}>
           <div>
         <h1 className="display-3 text-center" >Welcome to Maximum Quizzes</h1>
           {/* <Button color="info">Whatever we want this button to do</Button> */}
       </div>
       </Jumbotron>
+      </background>
     </div>
   );
 };
