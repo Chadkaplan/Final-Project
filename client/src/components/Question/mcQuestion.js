d9534ae51467929b59fdffa44751b74bc9dd650d
MAX_BLOOM <bloommax3@gmail.com>
2019-11-19 13:36:30 -0500

    fixed break

---
+++ client/src/components/Question/mcQuestion.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Row, Col} from "reactstrap";
+
+function mcQuestion (props){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default mcQuestion
